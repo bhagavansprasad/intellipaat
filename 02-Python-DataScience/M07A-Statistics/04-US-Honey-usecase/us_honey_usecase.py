c92b9f6f32ab7cacd9669ca13616b0cc7515d255
bhagavan <bhagavan@auranetworks.in>
2024-07-31 10:50:38 +0530

    Plotting graphs

--- 02-Python-DataScience/M07A-Statistics/04-US-Honey-usecase/us_honey_usecase.py
+++ 02-Python-DataScience/M07A-Statistics/04-US-Honey-usecase/us_honey_usecase.py
@@ -34,39 +34,126 @@ def us_honey_pair_plot():
     sns.pairplot(hf)
     plt.show()
 
-def multi_line_graph_01():
+def production_vs_stocks():
     hf = pd.read_csv("us_honey_dataset.csv")
     hf = hf.drop(columns=["Unnamed: 0"])
     hf.duplicated()
-    
+
     print(hf.head())
 
     sns.lineplot(x = 'year', y = 'production', data = hf, label='production', errorbar=None, marker='o')
     sns.lineplot(x = 'year', y = 'stocks', data = hf, label='stocks', errorbar=None, marker='*')
     sns.despine()
+    plt.title('Production vs Stocks')
     plt.grid(True)
     plt.show()
 
 
-def multi_line_graph_02():
+def production_vs_price():
     hf = pd.read_csv("us_honey_dataset.csv")
     hf = hf.drop(columns=["Unnamed: 0"])
     hf.duplicated()
+
+    print(hf.head())
     
-    hf = hf.drop("state", axis = 1).groupby("year").mean()
-    sns.lineplot(x = 'year', y = 'production', data = hf, label='production', errorbar=None, marker='o')
-    sns.lineplot(x = 'year', y = 'average_price', data = hf, label='average_price', errorbar=None, marker='*')
+    fig, ax1 = plt.subplots()
+    palette = sns.color_palette()
+
+    sns.lineplot(x='year', y='production', data=hf, ax=ax1, label='Production', errorbar=None, marker='o', color='blue')
+
+    ax2 = ax1.twinx()
+
+    sns.lineplot(x='year', y='average_price', data=hf, ax=ax2, label='Price', errorbar=None, marker='*', color='red')
+
     sns.despine()
+    plt.title('Production vs Price')
     plt.grid(True)
+    fig.tight_layout()  # to make sure the labels don't overlap
     plt.show()
+
+def colonies_and_yields():
+    hf = pd.read_csv("us_honey_dataset.csv")
+    hf = hf.drop(columns=["Unnamed: 0"])
+    hf.duplicated()
+
+    print(hf.head())
+    
+    fig, ax1 = plt.subplots()
+    palette = sns.color_palette()
+
+    sns.lineplot(x='year', y='colonies_number', data=hf, ax=ax1, label='colonies_number', errorbar=None, marker='o', color='blue')
+
+    ax2 = ax1.twinx()
+
+    sns.lineplot(x='year', y='yield_per_colony', data=hf, ax=ax2, label='yield_per_colony', errorbar=None, marker='*', color='red')
+
+    sns.despine()
+    plt.title('Colony count Vs Yield per Colony')
+    plt.grid(True)
+    fig.tight_layout()  # to make sure the labels don't overlap
+    plt.show()
+
+def colonies_vs_production():
+    hf = pd.read_csv("us_honey_dataset.csv")
+    hf = hf.drop(columns=["Unnamed: 0"])
+    hf.duplicated()
+
+    print(hf.head())
+    
+    fig, ax1 = plt.subplots()
+    palette = sns.color_palette()
+
+    sns.lineplot(x='year', y='colonies_number', data=hf, ax=ax1, label='colonies_number', errorbar=None, marker='o', color='blue')
+
+    ax2 = ax1.twinx()
+
+    sns.lineplot(x='year', y='production', data=hf, ax=ax2, label='production', errorbar=None, marker='*', color='red')
+
+    sns.despine()
+    plt.title('Colony count Vs Production')
+    plt.grid(True)
+    fig.tight_layout()  # to make sure the labels don't overlap
+    plt.show()
+
+def Multiple_graphs():
+    hf = pd.read_csv("us_honey_dataset.csv")
+    hf = hf.drop(columns=["Unnamed: 0"])
+    hf.duplicated()
+
+    print(hf.head())
+    
+    fig, axes = plt.subplots(1, 3, figsize=(18, 5))
+    fig.suptitle('US Honey Production Trend Over the years')
+    # palette = sns.color_palette()
+
+    sns.lineplot(ax = axes[0],         x='year', y='production', data=hf, label='Production', errorbar=None, marker='o', color='blue')
+    # ax2 = axes[0].twinx()
+    sns.lineplot(ax = axes[0].twinx(), x='year', y='average_price', data=hf, label='Price', errorbar=None, marker='*', color='red')
+
+    sns.lineplot(ax = axes[1],         x='year', y='colonies_number', data=hf, label='colonies_number', errorbar=None, marker='o', color='blue')
+    sns.lineplot(ax = axes[1].twinx(), x='year', y='yield_per_colony', data=hf, label='yield_per_colony', errorbar=None, marker='*', color='red')
+
+    sns.lineplot(ax = axes[2],         x='year', y='colonies_number', data=hf, label='colonies_number', errorbar=None, marker='o', color='red')
+    sns.lineplot(ax = axes[2].twinx(), x='year', y='production', data=hf, label='production', errorbar=None, marker='*', color='blue')
+    
+
+    sns.despine()
+    plt.title('Colony count Vs Production')
+    plt.grid(True)
+    fig.tight_layout()  # to make sure the labels don't overlap
+    plt.show()
+
 
 def main():
     # us_honey_scatter_graph_01()
     # us_honey_line_graph_01()
     # us_honey_pair_plot()
-    # multi_line_graph_01()
-    multi_line_graph_02()
-
+    # production_vs_stocks()
+    # production_vs_price()
+    # colonies_and_yields()
+    # colonies_vs_production()
+    Multiple_graphs()
+    
 if (__name__ == '__main__'):
     main()
 
